d504442602408052a55d2e831b7bd1c77f70111d
Emre ŞİŞMAN <emresisman53@gmail.com>
2022-10-31 12:45:07 +0300

    EntityFrameworkCore installed and DbContext created.

---
+++ MovieRecommendation.DAL/MovieRecommendationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MovieRecommendation.Entities;
+
+namespace MovieRecommendation.DAL
+{
+    public class MovieRecommendationDbContext : DbContext
+    {
+        public MovieRecommendationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Movies> Movies { get; set; }
+        public DbSet<MovieNotes> MovieNotes { get; set; }
+        public DbSet<MovieRatings> MovieRatings { get; set; }
+        public DbSet<Users> Users { get; set; }
+    }
+}
