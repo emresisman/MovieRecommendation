e53c8b42a0f51939239300ff46b6f04a5cb19f42
Emre ŞİŞMAN <emresisman53@gmail.com>
2022-11-01 15:07:58 +0300

    throw exception for not found movies.

--- MovieRecommendation.Business/Service/RatingNoteService.cs
+++ MovieRecommendation.Business/Service/RatingNoteService.cs
@@ -4,6 +4,7 @@ using MovieRecommendation.Business.Request;
 using MovieRecommendation.Business.Service.Interface;
 using MovieRecommendation.Entities;
 using System.Linq;
+using System;
 
 namespace MovieRecommendation.Business.Service
 {
@@ -11,17 +12,23 @@ namespace MovieRecommendation.Business.Service
     {
         private readonly IGenericRepository<MovieRatings> _ratingRepository;
         private readonly IGenericRepository<MovieNotes> _notesRepository;
+        private readonly IGenericRepository<Movies> _moviesRepository;
         private readonly IUserManager _userManager;
 
-        public RatingNoteService(IGenericRepository<MovieRatings> ratingRepository, IGenericRepository<MovieNotes> notesRepository, IUserManager userManager)
+        public RatingNoteService(IGenericRepository<MovieRatings> ratingRepository, IGenericRepository<MovieNotes> notesRepository, IUserManager userManager, IGenericRepository<Movies> moviesRepository)
         {
             _ratingRepository = ratingRepository;
             _notesRepository = notesRepository;
+            _moviesRepository = moviesRepository;
             _userManager = userManager;
         }
 
         public void SetRatingToMovie(RatingRequest ratingRequest)
         {
+            if (!_moviesRepository.GetQueryable().Where(x => x.Id == ratingRequest.MovieId).Any())
+            {
+                throw new Exception("Movie not found.");
+            }
             var entity = new MovieRatings
             {
                 MovieId = ratingRequest.MovieId,
@@ -38,9 +45,15 @@ namespace MovieRecommendation.Business.Service
                 _ratingRepository.Add(entity);
             }
             _ratingRepository.SaveChangesAsync();
+        
         }
+
         public void SetNoteToMovie(NoteRequest noteRequest)
         {
+            if (!_moviesRepository.GetQueryable().Where(x => x.Id == noteRequest.MovieId).Any())
+            {
+                throw new Exception("Movie not found.");
+            }
             var entity = new MovieNotes
             {
                 MovieId = noteRequest.MovieId,
